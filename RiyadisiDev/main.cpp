6acf4aa40f4c80f32303b98c21772c504ba41687
SankaD <sankadarshana@gmail.com>
2013-05-16 18:16:03 +0530

    Testing Project/ First Commit

---
+++ RiyadisiDev/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Sanka
+ *
+ * Created on May 16, 2013, 5:53 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
