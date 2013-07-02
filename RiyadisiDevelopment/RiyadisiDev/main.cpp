12d82513d6d29235058079c8230da7185c95c2a0
SankaD <sankadarshana@gmail.com>
2013-07-02 09:31:45 +0530

    Added resource files and some face detection algorithm into the main file

--- RiyadisiDevelopment/RiyadisiDev/main.cpp
+++ RiyadisiDevelopment/RiyadisiDev/main.cpp
@@ -1,23 +1,49 @@
-//#include "stdafx.h"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-#include <opencv\cv.h>
-#include <opencv\highgui.h>
-#include <opencv2\objdetect\objdetect.hpp>
+#include <iostream>
+#include <stdio.h>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main()
+/** Function Headers */
+void detectAndDisplay ( Mat frame );
+
+/** Global variables */
+String face_cascade_name = "Resources/Cascades/HaarCascades/haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "Resources/Cascades/HaarCascades/haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+string window_name = "Capture - Face detection";
+RNG rng ( 12345 );
+
+/** @function main */
+int main ( int argc, const char** argv )
 {
-    VideoCapture capture;
+    CvCapture* capture;
     Mat frame;
 
-    capture.open ( 0 );
-    if ( capture.isOpened() ) {
+    //-- 1. Load the cascades
+    if ( !face_cascade.load ( face_cascade_name ) ) {
+        printf ( "--(!)Error loading\n" );
+        return -1;
+    };
+    if ( !eyes_cascade.load ( eyes_cascade_name ) ) {
+        printf ( "--(!)Error loading\n" );
+        return -1;
+    };
+
+    //-- 2. Read the video stream
+    capture = cvCaptureFromCAM ( -1 );
+    if ( capture ) {
         while ( true ) {
-            capture >> frame;
+            frame = cvQueryFrame ( capture );
+
+            //-- 3. Apply the classifier to the frame
             if ( !frame.empty() ) {
-                imshow ( "Window", frame );
+                detectAndDisplay ( frame );
             } else {
                 printf ( " --(!) No captured frame -- Break!" );
                 break;
@@ -29,4 +55,37 @@ int main()
             }
         }
     }
+    return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay ( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+
+    cvtColor ( frame, frame_gray, CV_BGR2GRAY );
+    equalizeHist ( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale ( frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size ( 30, 30 ) );
+
+    for ( int i = 0; i < faces.size(); i++ ) {
+        Point center ( faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5 );
+        ellipse ( frame, center, Size ( faces[i].width * 0.5, faces[i].height * 0.5 ), 0, 0, 360, Scalar ( 255, 0, 255 ), 4, 8, 0 );
+
+        Mat faceROI = frame_gray ( faces[i] );
+        std::vector<Rect> eyes;
+
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale ( faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size ( 30, 30 ) );
+
+        for ( int j = 0; j < eyes.size(); j++ ) {
+            Point center ( faces[i].x + eyes[j].x + eyes[j].width * 0.5, faces[i].y + eyes[j].y + eyes[j].height * 0.5 );
+            int radius = cvRound ( ( eyes[j].width + eyes[j].height ) * 0.25 );
+            circle ( frame, center, radius, Scalar ( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow ( window_name, frame );
 }
