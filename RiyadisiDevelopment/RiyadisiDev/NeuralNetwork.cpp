64e1a33baaf2f29355f3b3800b1a5f8da1fecc6c
SankaD <sankadarshana@gmail.com>
2013-10-28 13:38:15 +0530

    Integraded FANN into the project. Changed project properties (FANN)

---
+++ RiyadisiDevelopment/RiyadisiDev/NeuralNetwork.cpp
@@ -0,0 +1,57 @@
+#include "NeuralNetwork.h"
+
+NeuralNetwork::NeuralNetwork ( bool isTraining, bool fromFile )
+{
+
+    neuralDataFilename = "neuralData.neural";
+
+    this->isTraining = isTraining;
+
+    if ( isTraining ) {
+        // if the data file is available resume from there.otherwise create a new network
+        if ( fromFile ) {
+            network.create_from_file ( neuralDataFilename );
+        } else {
+            network.create_standard ( numLayers, numInput, numNeuronsHidden, numOutput );
+        }
+        network.set_activation_function_hidden ( FANN::SIGMOID_SYMMETRIC );
+        network.set_activation_function_output ( FANN::SIGMOID_SYMMETRIC );
+    } else {
+        network.create_from_file ( neuralDataFilename );
+    }
+}
+NeuralNetwork::~NeuralNetwork()
+{
+    network.destroy();
+}
+void NeuralNetwork::trainNetwork ( string fileName )
+{
+    if ( !isTraining ) {
+        throw exception ( "Unsupported mode" );
+    }
+
+    network.train_on_file ( fileName, maxEpochs, epochsBetweenReports, desiredError );
+    network.save ( neuralDataFilename );
+}
+void NeuralNetwork::resetNetwork()
+{
+    throw exception ( "Not implemented yet" );
+}
+bool NeuralNetwork::getAlertValue ( double weightedPerclose, double noddingOffMeasure,
+                                    double gazeMeasure, double headRotationMeasure,
+                                    double yawningMeasure )
+{
+    if ( isTraining ) {
+        throw exception ( "Unsupported mode" );
+    }
+    fann_type input[5], output;
+    input[0] = weightedPerclose;
+    input[1] = noddingOffMeasure;
+    input[2] = gazeMeasure;
+    input[3] = headRotationMeasure;
+    input[4] = yawningMeasure;
+
+    output = * ( network.run ( input ) );
+
+    return ( output > 0.5 );
+}
--- RiyadisiDevelopment/RiyadisiDev/NeuralNetwork.h
+++ RiyadisiDevelopment/RiyadisiDev/NeuralNetwork.h
@@ -1,22 +1,54 @@
 #include "include\fann.h"
 #include "include\fann_cpp.h"
+#include "include\floatfann.h"
 #include <iostream>
 
 using namespace std;
+using namespace FANN;
+
+static const unsigned int numInput = 5;
+static const unsigned int numOutput = 1;
+static const unsigned int numLayers = 3;
+static const unsigned int numNeuronsHidden = 3;
+static const float desiredError = 0.0001;
+static const unsigned int maxEpochs = 500000;
+static const unsigned int epochsBetweenReports = 1000;
 
 ///<summary>
 ///Represents a neural network
 ///</summary>
 class NeuralNetwork {
 public:
-    NeuralNetwork();
+    ///<summary>
+    ///Creates a neural network
+    ///</summary>
+    ///<param name="isTraining">Whether the network is created for training or not.</param>
+    NeuralNetwork ( bool isTraining = false, bool fromFile = true );
     ~NeuralNetwork();
 private:
+    bool isTraining;
+    string neuralDataFilename;;// = "neuralData.neural";
+
+
+    neural_net network;
+
     ///<summary>
     ///Trains the network using the given file.
     ///</summary>
     void trainNetwork ( string fileName );
 
-    double getValue ( double weightedPerclose, double noddingOffValue,
-                      double gazeValue, double headRotation, double yawningValue );
-}
+    ///<summary>
+    ///Clears the weights and resets the neural network
+    ///</summary>
+    void resetNetwork();
+
+    ///<summary>
+    ///Gets a value representing the necessity of alerting the driver.
+    ///</summary>
+    ///<param name="weightedPerclose">Accumilated weighted PERCLOSE value at the moment</param>
+    ///<param name="noddingOffMeasure">Accumilated weighted Nodding off values</param>
+    ///<returns> Whether or not to alert the driver</returns>
+    bool getAlertValue ( double weightedPerclose, double noddingOffMeasure,
+                         double gazeMeasure, double headRotationMeasure,
+                         double yawningMeasure );
+};
